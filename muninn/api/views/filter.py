dc6d6792d6a3f46d1403aa379edeaa3a18ef82d3
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-08-03 11:14:57 +0300

    refactored filter

--- muninn/api/views/filter.py
+++ muninn/api/views/filter.py
@@ -5,37 +5,31 @@ class CaseInsensitiveFilter(DjangoFilterBackend):
     """ Filter that allows url params to be case insensitive """
 
     def filter_queryset(self, request, queryset, view):
-        param = request.query_params.get('status', None)
-        if param is not None:
-            data = queryset.filter(status__iexact=param)
-            return data
-        param = request.query_params.get('industry', None)
-        if param is not None:
-            data = queryset.filter(industry__iexact=param)
-            return data
-        param = request.query_params.get('developer', None)
-        if param is not None:
-            data = queryset.filter(developer__name__iexact=param)
-            return data
-        param = request.query_params.get('publisher', None)
-        if param is not None:
-            data = queryset.filter(publisher__name__iexact=param)
-            return data
-        param = request.query_params.get('engine', None)
-        if param is not None:
-            data = queryset.filter(engine__name__iexact=param)
-            return data
-        param = request.query_params.get('series', None)
-        if param is not None:
-            data = queryset.filter(series__iexact=param)
-            return data
-        param = request.query_params.get('genre', None)
-        if param is not None:
-            data = queryset.filter(genre__iexact=param)
-            return data
-        param = request.query_params.get('platforms', None)
-        if param is not None:
-            data = queryset.filter(platforms__contains=param)
-            return data
+        url_param = list(request.query_params.keys())
+        value = [request.query_params[value] for value in url_param]
+
+        if 'status' in url_param:
+            return queryset.filter(status__iexact=value[0])
+
+        if 'inudstry' in url_param:
+            return queryset.filter(industry__iexact=value[0])
+
+        if 'developer' in url_param:
+            return queryset.filter(developer__name__iexact=value[0])
+
+        if 'publisher' in url_param:
+            return queryset.filter(publisher__name__iexact=value[0])
+
+        if 'engine' in url_param:
+            return queryset.filter(engine__name__iexact=value[0])
+
+        if'series' in url_param:
+            return queryset.filter(series__iexact=value[0])
+
+        if 'genre' in url_param:
+            return queryset.filter(genre__iexact=value[0])
+
+        if 'platforms' in url_param:
+            return queryset.filter(platforms__contains=value[0])
 
         return queryset
